d2a6fe0d6b0d5e5a716346d3c321b0302707527b
Abdulrehman <abdulrehman4613895-uigit config --global user.email abdulrehman4613895@gmail.com>
2026-02-19 03:21:57 +0500

    change for done

--- src/component/Productcardthree.jsx
+++ src/component/Productcardthree.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import imag21 from "../assets/home4-banner1.webp"
 import imag22 from "../assets/home4-banner2.webp"
+import imag23 from "../assets/home5-block-img.webp"
 
 export default function Productcardthree() {
   return (
     <div>
       <div className='collection'>
-        <img src="src/assets/home5-block-img.webp" alt="" />
+        <img src={imag23} alt="" />
         <h1>A UNIQUE FASHION TUORING</h1>
         <h2>WELCOME TO STORE</h2>
         <p>Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam <br /> littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta <br /> decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</p>
